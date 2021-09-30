8c767e07eff30f27dee4b512fb537231a0bd089d
Tomas Kuprinskas <t.kuprinskas@gmail.com>
2021-09-30 11:29:34 +0300

    Update mygroups.js

--- mygroups.js
+++ mygroups.js
@@ -1,5 +1,5 @@
 // const urlContent = "http://localhost:3000/v1/accounts/accounts";
-const urlContent = "https://split-bills-backend-5naym.ondigitalocean.app/accounts";
+const urlContent = "https://split-bills-backend-5naym.ondigitalocean.app/v1/accounts/accounts";
 const token = window.localStorage.getItem("token");
 
 showData();
